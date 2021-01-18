5366fe0943b1d4a78197a4b883570542523c23a5
MACE <d.mace@live.com>
2021-01-18 20:49:08 +0300

    * renamed project

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.IDP/Startup.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.IDP/Startup.cs
@@ -3,12 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Exadel.CrazyPrice.IDP
+namespace Exadel.CrazyPrice.IdentityServer
 {
     public class Startup
     {
