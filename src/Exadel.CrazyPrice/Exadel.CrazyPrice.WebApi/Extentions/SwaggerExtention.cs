d90244310c0dbf24b42834ae0fcf0b439f82eb14
MACE <d.mace@live.com>
2021-01-18 16:29:08 +0300

    + created the Solution structure

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.WebApi/Extentions/SwaggerExtention.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Exadel.CrazyPrice.WebApi.Extentions
+{
+    internal static class SwaggerExtention
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CrazyPrice API",
+                    Description = "Description Web API for the site Crazy Price.",
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT license",
+                        Url = new Uri("https://opensource.org/licenses/MIT"),
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerCP(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrazyPrice WebApi v1"));
+
+            return app;
+        }
+    }
+}
---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.WebApi/Startup.cs
@@ -0,0 +1,62 @@
+using Exadel.CrazyPrice.WebApi.Extentions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Exadel.CrazyPrice.WebApi
+{
+    /// <summary>
+    /// Determines Startup configuration.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Creates Startup configuration.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets Configuration
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwagger();
+        }
+
+        /// <summary>
+        /// Gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwaggerCP();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
