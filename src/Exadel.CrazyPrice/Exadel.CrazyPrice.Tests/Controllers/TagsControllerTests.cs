9bd2333b4ac6d8207e1f2cde29adebf2ade7724b
MACE <d.mace@live.com>
2021-01-19 01:33:08 +0300

    * changed TagsController

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Controllers/TagsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Exadel.CrazyPrice.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exadel.CrazyPrice.WebApi.Controllers.Tests
+{
+    public class TagsControllerTests
+    {
+        [Fact()]
+        public void GetTest()
+        {
+             
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
