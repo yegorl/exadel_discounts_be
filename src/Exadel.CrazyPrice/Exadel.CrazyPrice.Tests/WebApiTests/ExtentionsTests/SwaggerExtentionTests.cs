ecd6fcad2c1f0d93754a6124c77937ffe48f72a0
MACE <d.mace@live.com>
2021-01-19 11:10:42 +0300

    + added UseSwaggerCrazyPriceTest

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/WebApiTests/ExtentionsTests/SwaggerExtentionTests.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/WebApiTests/ExtentionsTests/SwaggerExtentionTests.cs
@@ -1,5 +1,6 @@
 using Exadel.CrazyPrice.WebApi.Extentions;
 using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
@@ -7,13 +8,14 @@ namespace Exadel.CrazyPrice.Tests
 {
     public class SwaggerExtentionTests
     {
+        readonly IServiceCollection _service = new ServiceCollection();
+
         [Fact]
         public void AddSwaggerTest()
         {
             const string nameSwaggerGenerator = "SwaggerGenerator";
 
-            var service = new ServiceCollection();
-            var addSwagger = service.AddSwagger();
+            var addSwagger = _service.AddSwagger();
             foreach (var serviceDescriptor in addSwagger)
             {
                 if (serviceDescriptor?.ImplementationType?.Name == nameSwaggerGenerator)
@@ -23,6 +25,28 @@ namespace Exadel.CrazyPrice.Tests
                 }
             }
             Assert.True(false, $"ServiceCollection is not contains {nameSwaggerGenerator}.");
+        }
+
+        [Fact]
+        public void UseSwaggerCrazyPriceTest()
+        {
+            _service.AddSwagger();
+            var appBuilder = new ApplicationBuilder(_service.BuildServiceProvider());
+            appBuilder.UseSwaggerCrazyPrice();
+            try
+            {
+                var app = appBuilder.Build();
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                if (ex.Message.Contains("Swashbuckle.AspNetCore.SwaggerUI.SwaggerUIMiddleware"))
+                {
+                    Assert.True(true);
+                    return;
+                };
+            }
+
+            Assert.True(false, $"IApplicationBuilder is not contains SwaggerUIMiddleware.");
         }
     }
 }
