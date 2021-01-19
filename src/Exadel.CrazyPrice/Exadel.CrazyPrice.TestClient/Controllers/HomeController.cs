10b3186187866d5133c596aa94d249f6095d897f
kaspianturbo@gmail.com <kaspianturbo@gmail.com>
2021-01-19 18:03:59 +0200

    +Added test Client  that use API

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.TestClient/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Exadel.CrazyPrice.TestClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Exadel.CrazyPrice.TestClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = _httpClientFactory.CreateClient("CrazyPriceAPI");
+            var response = await data.GetAsync("https://localhost:44389/tags");
+            var message = await response.Content.ReadAsStringAsync();
+            ViewBag.Message = message;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
