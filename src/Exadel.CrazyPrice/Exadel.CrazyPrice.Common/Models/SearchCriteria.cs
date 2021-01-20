d423669c49c160e0c92d7b41f23b7e7064a03a67
MACE <d.mace@live.com>
2021-01-20 16:35:51 +0300

    + added Guid SearchPersonId

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Models/SearchCriteria.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Models/SearchCriteria.cs
@@ -1,4 +1,6 @@
-﻿namespace Exadel.CrazyPrice.Common.Models
+﻿using System;
+
+namespace Exadel.CrazyPrice.Common.Models
 {
     public class SearchCriteria
     {      
@@ -8,18 +10,23 @@
         public string SearchText { get; set; }
 
         /// <summary>
-        /// Gets or sets a Country.
+        /// Gets or sets a search Country.
         /// </summary>
         public string SearchCountry { get; set; }
 
         /// <summary>
-        /// Gets or sets a City.
+        /// Gets or sets a search City.
         /// </summary>
         public string SearchCity { get; set; }
 
         /// <summary>
-        /// Gets or sets a Discount Program option.
+        /// Gets or sets a search Discount Program option.
         /// </summary>
         public DiscountOption SearchDiscountOption { get; set; }
+
+        /// <summary>
+        /// Gets or sets a search Person Id.
+        /// </summary>
+        public Guid SearchPersonId { get; set; }
     }
 }
