e73e409506afd51192916b676662b3ead883d233
MACE <d.mace@live.com>
2021-01-20 11:02:57 +0300

    + added ReplaceTwoAndMoreSpaceByOneTest

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Extentions/StringExtentionTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Exadel.CrazyPrice.Common.Extentions.Tests
+{
+    public class StringExtentionTests
+    {
+        [Theory]
+        [InlineData("ReplaceSpaceTest", "ReplaceSpaceTest")]
+        [InlineData(" Replace Space Test ", "Replace Space Test")]
+        [InlineData("    Replace Space     Test ", "Replace Space Test")]
+        [InlineData("    ReplaceSpaceTest     ", "ReplaceSpaceTest")]
+        public void ReplaceTwoAndMoreSpaceByOneTest(string value, string expectedResult)
+        {
+            value.ReplaceTwoAndMoreSpaceByOne().Should().Be(expectedResult);
+        }
+    }
+}
