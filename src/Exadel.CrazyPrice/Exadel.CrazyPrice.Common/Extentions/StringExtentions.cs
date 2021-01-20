e2779482a4a0df23c5337496fe124d74641d6a26
MACE <d.mace@live.com>
2021-01-20 22:04:01 +0300

    * changed extentions. Now faster than RegExp (from test time). Added more tests.

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/StringExtentions.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/StringExtentions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text;
 
 namespace Exadel.CrazyPrice.Common.Extentions
 {
@@ -15,8 +16,38 @@ namespace Exadel.CrazyPrice.Common.Extentions
         /// <returns></returns>
         public static string ReplaceTwoAndMoreSpaceByOne(this string s)
         {
-            return string.IsNullOrEmpty(s) ? s : Regex.Replace(s, " {2,}", " ").Trim();
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            s = s.Trim();
+            if (!s.Contains("  "))
+            {
+                return s;
+            }
+
+            s = ' ' + s;
+            var stbBuilder = new StringBuilder();
+            for (var i = 1; i <= s.Length - 1; i++)
+            {
+                if (s[i] == ' ' && s[i - 1] == ' ')
+                {
+                    continue;
+                }
+                stbBuilder.Append(s[i]);
+            }
+            return stbBuilder.ToString();
         }
 
+        /// <summary>
+        /// Gets words separated by a space.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string GetOnlyLettersDigitsAndOneSpace(this string s) =>
+            s
+                .Where(c => char.IsLetter(c) || char.IsDigit(c) || c == ' ')
+                .Aggregate(string.Empty, (current, c) => current + c).ReplaceTwoAndMoreSpaceByOne();
     }
 }
--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Extentions/StringExtentionTests.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Extentions/StringExtentionTests.cs
@@ -4,9 +4,12 @@ using Xunit;
 
 namespace Exadel.CrazyPrice.Tests.Common.Extentions
 {
-    public class StringExtentionTests
+    public class StringExtentionsTests
     {
         [Theory]
+        [InlineData("", "")]
+        [InlineData(" ", "")]
+        [InlineData(null, null)]
         [InlineData("ReplaceSpaceTest", "ReplaceSpaceTest")]
         [InlineData(" Replace Space Test ", "Replace Space Test")]
         [InlineData("    Replace Space     Test ", "Replace Space Test")]
@@ -14,6 +17,14 @@ namespace Exadel.CrazyPrice.Tests.Common.Extentions
         public void ReplaceTwoAndMoreSpaceByOneTest(string value, string expectedResult)
         {
             value.ReplaceTwoAndMoreSpaceByOne().Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData("RemoveNonLette.,:;&$%()-+]+$rAndNonDigit/\\", "RemoveNonLetterAndNonDigit")]
+        [InlineData(" Replace Space\n  \rTest\v", "Replace Space Test")]
+        public void GetOnlyLettersDigitsAndOneSpaceTest(string value, string expectedResult)
+        {
+            value.GetOnlyLettersDigitsAndOneSpace().Should().Be(expectedResult);
         }
     }
 }
