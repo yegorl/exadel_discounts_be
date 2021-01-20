9b2f31c2096f48061fb27c8da31be3ef5ae22359
MACE <d.mace@live.com>
2021-01-20 10:18:57 +0300

    + added extention string

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/StringExtention.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exadel.CrazyPrice.Common.Ensure
+{
+    /// <summary>
+    /// Represents correcting string.
+    /// </summary>
+    public static class StringExtention
+    {
+
+        /// <summary>
+        /// Removes 2 and more space. Trim.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ReplaceSpace(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            return Regex.Replace(s, " {2,}", " ").Trim();
+        }
+
+    }
+}
