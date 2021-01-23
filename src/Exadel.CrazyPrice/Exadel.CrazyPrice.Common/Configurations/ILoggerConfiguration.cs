ac9d0b3fe6ee48b4bc9cd027eec9f47fb7861865
MACE <d.mace@live.com>
2021-01-23 11:30:29 +0300

    - removed ILoggerConfiguration

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Configurations/ILoggerConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Exadel.CrazyPrice.Common.Configurations
-{
-    public interface ILoggerConfiguration<out T> where T : class, new()
-    {
-        public T LoggerConfiguration { get; }
-    }
-}
