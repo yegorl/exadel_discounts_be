0387ea9b8efa624f93626bf9779d78bdc43374c2
MACE <d.mace@live.com>
2021-01-24 16:04:04 +0300

    * updated extentions and their tests

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/EnumExtentions.cs
@@ -0,0 +1,46 @@
+﻿using Exadel.CrazyPrice.Common.Models.Option;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exadel.CrazyPrice.Common.Extentions
+{
+    public static class EnumExtentions
+    {
+        /// <summary>
+        /// Gets the formatted string from enum.
+        /// $k or $K - the key enum,
+        /// $v or $V - the value enum.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="formatString"></param>
+        /// <returns></returns>
+        public static string GetStringFrom<T>(string formatString) where T : struct
+        {
+            if (string.IsNullOrEmpty(formatString))
+            {
+                throw new ArgumentNullException(formatString);
+            }
+
+            formatString = formatString.GetValidContent(CharOptions.Letter | CharOptions.Symbol, " :-=|").ToLowerInvariant();
+
+            var valid = new[] { "$k",  "$v" };
+            if (!valid.Any(s => formatString.Contains(s)))
+            {
+                throw new FormatException(formatString);
+            }
+
+            var kvp =
+                (from object value in Enum.GetValues(typeof(T)) select new KeyValuePair<string, int>(((T)value).ToString(), (int)(object)(T)value)).ToList();
+
+            var strBuilder = new StringBuilder();
+            foreach (var (key, value) in kvp)
+            {
+                strBuilder.Append(formatString.Replace("$k", key).Replace("$v", value.ToString()) + ", ");
+            }
+
+            return strBuilder.ToString().TrimEnd(',', ' ');
+        }
+    }
+}
--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/StringExtentions.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Extentions/StringExtentions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using Exadel.CrazyPrice.Common.Models.Option;
 
 namespace Exadel.CrazyPrice.Common.Extentions
 {
@@ -10,48 +12,6 @@ namespace Exadel.CrazyPrice.Common.Extentions
     /// </summary>
     public static class StringExtentions
     {
-
-        /// <summary>
-        /// Removes 2 and more space. Trim.
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        public static string ReplaceTwoAndMoreSpaceByOne(this string s)
-        {
-            if (string.IsNullOrEmpty(s))
-            {
-                return s;
-            }
-
-            s = s.Trim();
-            if (!s.Contains("  "))
-            {
-                return s;
-            }
-
-            s = ' ' + s;
-            var stbBuilder = new StringBuilder();
-            for (var i = 1; i <= s.Length - 1; i++)
-            {
-                if (s[i] == ' ' && s[i - 1] == ' ')
-                {
-                    continue;
-                }
-                stbBuilder.Append(s[i]);
-            }
-            return stbBuilder.ToString();
-        }
-
-        /// <summary>
-        /// Gets words separated by a space.
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        public static string GetOnlyLettersDigitsAndOneSpace(this string s) =>
-            s
-                .Where(c => char.IsLetter(c) || char.IsDigit(c) || c == ' ')
-                .Aggregate(string.Empty, (current, c) => current + c).ReplaceTwoAndMoreSpaceByOne();
-
         /// <summary>
         /// Gets DateTime from a string with format "dd.MM.yyyy HH:mm:ss".
         /// </summary>
@@ -81,6 +41,147 @@ namespace Exadel.CrazyPrice.Common.Extentions
 
             var chars = new[] { '0', '1' };
             return stringWorkingDays.All(stringWorkingDay => chars.Contains(stringWorkingDay));
+        }
+        
+        /// <summary>
+        /// Gets string what two and more chars replaced by some one.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="chars"></param>
+        /// <returns></returns>
+        public static string ReplaceTwoAndMoreCharsBySomeOne(this string s, string chars)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            return s.ReplaceTwoAndMoreCharsBySomeOne(chars.ToCharArray());
+        }
+
+        /// <summary>
+        /// Gets string what two and more chars replaced by some one.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="chars"></param>
+        /// <returns></returns>
+        public static string ReplaceTwoAndMoreCharsBySomeOne(this string s, char[] chars)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            if (chars == null || chars.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(chars));
+            }
+
+            s = s.Trim(chars);
+
+            if (s.Length == 1 && chars.Contains(s[0]))
+            {
+                return string.Empty;
+            }
+
+            var charsBase = s.ToCharArray();
+
+            foreach (var c in chars)
+            {
+                for (var i = 1; i <= charsBase.Length - 2; i++)
+                {
+                    if (charsBase[i] == char.MinValue || charsBase[i - 1] != c || charsBase[i] != c)
+                    {
+                        continue;
+                    }
+
+                    charsBase[i - 1] = char.MinValue;
+                }
+            }
+
+            var strBuilder = new StringBuilder(charsBase.Length);
+            foreach (var c in charsBase)
+            {
+                if (c == char.MinValue)
+                {
+                    continue;
+                }
+
+                strBuilder.Append(c);
+            }
+            return strBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Gets valid content with chars from CharOptions and specialChars.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="charOptions"></param>
+        /// <param name="specialChars"></param>
+        /// <returns></returns>
+        public static string GetValidContent(this string s, CharOptions charOptions, string specialChars = null)
+        {
+            if (string.IsNullOrEmpty(s)) return string.Empty;
+            
+            var rulesForChar = BuildRulesForChar(charOptions);
+            if (!string.IsNullOrEmpty(specialChars))
+            {
+                rulesForChar.Add(specialChars.Contains);
+            }
+
+            var result = s
+                .Where(c => rulesForChar.Any(func => func(c)))
+                .Aggregate(string.Empty, (current, c) => current + c);
+
+            return string.IsNullOrEmpty(specialChars) ? result : result.ReplaceTwoAndMoreCharsBySomeOne(specialChars);
+        }
+
+        private static List<Func<char, bool>> BuildRulesForChar(CharOptions charOptions)
+        {
+            var rulesForChar = new List<Func<char, bool>>();
+
+            if ((charOptions & CharOptions.Letter) == CharOptions.Letter)
+            {
+                rulesForChar.Add(char.IsLetter);
+            }
+            if ((charOptions & CharOptions.Upper) == CharOptions.Upper)
+            {
+                rulesForChar.Add(char.IsUpper);
+            }
+            if ((charOptions & CharOptions.Lower) == CharOptions.Lower)
+            {
+                rulesForChar.Add(char.IsLower);
+            }
+            if ((charOptions & CharOptions.Digit) == CharOptions.Digit)
+            {
+                rulesForChar.Add(char.IsDigit);
+            }
+            if ((charOptions & CharOptions.Number) == CharOptions.Number)
+            {
+                rulesForChar.Add(char.IsNumber);
+            }
+            if ((charOptions & CharOptions.Separator) == CharOptions.Separator)
+            {
+                rulesForChar.Add(char.IsSeparator);
+            }
+            if ((charOptions & CharOptions.WhiteSpace) == CharOptions.WhiteSpace)
+            {
+                rulesForChar.Add(char.IsWhiteSpace);
+            }
+            if ((charOptions & CharOptions.Symbol) == CharOptions.Symbol)
+            {
+                rulesForChar.Add(char.IsSymbol);
+            }
+            if ((charOptions & CharOptions.Control) == CharOptions.Control)
+            {
+                rulesForChar.Add(char.IsControl);
+            }
+            if ((charOptions & CharOptions.Punctuation) == CharOptions.Punctuation)
+            {
+                rulesForChar.Add(char.IsPunctuation);
+            }
+
+            return rulesForChar;
         }
     }
 }
