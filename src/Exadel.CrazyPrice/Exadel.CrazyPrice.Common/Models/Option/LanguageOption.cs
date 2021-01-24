054c011ea51c54a49f19d6ca2503ec10fd0b5484
MACE <d.mace@live.com>
2021-01-24 16:02:37 +0300

    * updated the structure of folder models

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Common/Models/Option/LanguageOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Exadel.CrazyPrice.Common.Models.Option
+{
+    /// <summary>
+    /// Represents the supported languages.
+    /// </summary>
+    public enum LanguageOption
+    {
+        /// <summary>
+        /// English
+        /// </summary>
+        En,
+        /// <summary>
+        /// Russian
+        /// </summary>
+        Ru
+    }
+}
