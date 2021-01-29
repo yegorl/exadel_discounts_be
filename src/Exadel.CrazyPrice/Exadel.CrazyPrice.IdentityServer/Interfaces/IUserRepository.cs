4b3eb5ca824191bd7e15cb874192cdb73876663f
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-01-29 22:50:30 +0200

    +Created interface IUserRepository and UserRepository implementation

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/Interfaces/IUserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exadel.CrazyPrice.IdentityServer.Models;
+
+namespace Exadel.CrazyPrice.IdentityServer.Interfaces
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Get user by user e-mail
+        /// </summary>
+        /// <param name="email">E-mail of user</param>
+        /// <returns>User or null if not found</returns>
+        Task<CustomUser> GetUserByEmail(string email);
+
+        /// <summary>
+        /// Get user by Uid
+        /// </summary>
+        /// <param name="userUid">Uid of user</param>
+        /// <returns>User or null if not found</returns>
+        Task<CustomUser> GetUserByUid(string userUid);
+
+        /// <summary>
+        /// Validate credentials by checking email and password
+        /// </summary>
+        /// <param name="email">E-mail of user</param>
+        /// <param name="password">Password of user</param>
+        /// <returns></returns>
+        Task<bool> ValidateCredentials(string email, string password);
+    }
+}
---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exadel.CrazyPrice.IdentityServer.Interfaces;
+using Exadel.CrazyPrice.IdentityServer.Models;
+
+namespace Exadel.CrazyPrice.IdentityServer.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IEnumerable<CustomUser> _users;
+        public UserRepository()
+        {
+            
+        }
+
+        public async Task<bool> ValidateCredentials(string email, string password)
+        {
+            var user = await GetUserByEmail(email);
+
+            if (user != null)
+            {
+                if (string.IsNullOrWhiteSpace(user.Password) && string.IsNullOrWhiteSpace(password))
+                {
+                    return true;
+                }
+                return user.Password.Equals(password);
+            }
+            return false;
+        }
+
+        public async Task<CustomUser> GetUserByEmail(string email)
+        {
+            return await Task.FromResult(_users.FirstOrDefault(x => x.Email == email));
+        }
+
+        public async Task<CustomUser> GetUserByUid(string userId)
+        {
+            return await Task.FromResult(_users.FirstOrDefault(x => x.SubjectId == userId));
+        }
+    }
+}
