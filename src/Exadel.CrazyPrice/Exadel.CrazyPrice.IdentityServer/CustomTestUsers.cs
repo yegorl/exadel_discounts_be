ac1335eb7c423172240356ad51a08e4c9a07039d
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-01-30 15:32:44 +0200

    *changed password in TestUsers

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/CustomTestUsers.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/CustomTestUsers.cs
@@ -16,7 +16,7 @@ namespace Exadel.CrazyPrice.IdentityServer
                     SubjectId = "97b248a1-c706-4987-9b31-101429ec2aff",
                     Username = "Frank",
                     Email = "Frank@gmail.com",
-                    Password = "1111",
+                    Password = "W1BKFpkamZaLjBiULqea/DQTDNa1yCa+|8312|qMys3s2rN6+pLboyOOUxvdeNd1TjiNEH",
                     Role = "employee"
                 },
                 new CustomUser
@@ -24,7 +24,7 @@ namespace Exadel.CrazyPrice.IdentityServer
                     SubjectId = "13b4e6a3-42f2-4a43-be4e-7fc15454a7ab",
                     Username = "Claire",
                     Email = "Claire@gmail.com",
-                    Password = "2222",
+                    Password = "x19Vo3PvwSCarSwya+fde2EcmeXGmPlI|8312|rC4jenVp7jGG2X+uHcR7mweVyG6+pTsT",
                     Role = "moderator"
                 },
                 new CustomUser
@@ -32,7 +32,7 @@ namespace Exadel.CrazyPrice.IdentityServer
                     SubjectId = "10c1a0a1-556a-475f-9597-c705dc1cc48b",
                     Username = "Bob",
                     Email = "Bob@gmail.com",
-                    Password = "3333",
+                    Password = "+MQ8pPkNkxj6WoC0Yac2QVb149z39wc3|8312|pYALJHKH92W5zKchwQ5jDaFP8WIuZI9t",
                     Role = "admin"
                 }
             };
