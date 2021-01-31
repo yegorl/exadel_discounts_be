8e98d34ece2a6f0d65f7a089b5a2c9b25acb25b3
MACE <d.mace@live.com>
2021-01-31 13:45:43 +0300

    + added DbContext

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Data/Context/IDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Exadel.CrazyPrice.Data.Models;
+using MongoDB.Driver;
+
+namespace Exadel.CrazyPrice.Data.Context
+{
+    public interface IDbContext
+    {
+        public IMongoCollection<DbAddress> Addresses { get; }
+
+        public IMongoCollection<DbCompany> Companies { get; }
+
+        public IMongoCollection<DbDiscount> Discounts { get; }
+
+        public IMongoCollection<DbPerson> Persons { get; }
+
+        public IMongoCollection<DbTag> Tags { get; }
+    }
+}
---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Data/Context/MongoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Exadel.CrazyPrice.Common.Configurations;
+using Exadel.CrazyPrice.Data.Models;
+using MongoDB.Driver;
+
+namespace Exadel.CrazyPrice.Data.Context
+{
+    public class MongoDbContext : IDbContext
+    {
+        private readonly IMongoDatabase _db;
+
+        public MongoDbContext(IDbConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.ConnectionString);
+            _db = client.GetDatabase(configuration.Database);
+        }
+
+        public IMongoCollection<DbAddress> Addresses => _db.GetCollection<DbAddress>("Addresses");
+
+        public IMongoCollection<DbCompany> Companies => _db.GetCollection<DbCompany>("Companies");
+
+        public IMongoCollection<DbDiscount> Discounts => _db.GetCollection<DbDiscount>("Discounts");
+
+        public IMongoCollection<DbPerson> Persons => _db.GetCollection<DbPerson>("Persons");
+
+        public IMongoCollection<DbTag> Tags => _db.GetCollection<DbTag>("Tags");
+    }
+}
