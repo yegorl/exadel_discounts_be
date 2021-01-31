dcf639e9a6e55abc0db42c07a6cbb5c1eb3c8040
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-01-31 11:10:12 +0200

    *removed client side hashing

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/wwwroot/js/hash-password.js
+++
@@ -1,8 +0,0 @@
-const password_input = document.querySelector("#password");
-const hash_input = document.querySelector("#hash");
-const but_login = document.querySelector("#but_login");
-
-but_login.addEventListener('click', () => {
-    let value = password_input.value;
-    hash_input.value = sha256(value);
-});
