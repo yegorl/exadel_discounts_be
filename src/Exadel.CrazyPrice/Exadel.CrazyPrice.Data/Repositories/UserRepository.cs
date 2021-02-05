f10673f456dd2b9f5ec340b1e7b3120f6491524e
MACE <d.mace@live.com>
2021-02-05 17:46:23 +0300

    + added implementation UserRepository

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Data/Repositories/UserRepository.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Data/Repositories/UserRepository.cs
@@ -20,14 +20,44 @@ namespace Exadel.CrazyPrice.Data.Repositories
             _users = db.GetCollection<DbUser>("Users");
         }
 
-        public Task<User> GetUserByEmailAsync(string mail)
+        public async Task<User> GetUserByEmailAsync(string mail)
         {
-            throw new NotImplementedException();
+            return await GetUserAsync("{ \"mail\" : \"" + mail + "\" }");
         }
 
-        public Task<User> GetUserByUidAsync(Guid uid)
+        public async Task<User> GetUserByUidAsync(Guid uid)
         {
-            throw new NotImplementedException();
+            return await GetUserAsync("{ \"_id\" : \"" + uid + "\" }");
+        }
+
+        private async Task<User> GetUserAsync(string query)
+        {
+            var users = await _users.FindSync(query,
+                new FindOptions<DbUser> { Limit = 1 }).ToListAsync();
+
+            if (users == null || users.Count == 0)
+            {
+                return new User();
+            }
+
+            var isGuid = Guid.TryParse(users[0].Id, out var guid);
+
+            if (!isGuid)
+            {
+                return new User();
+            }
+
+            return new User
+            {
+                Id = guid,
+                Name = users[0].Name,
+                Surname = users[0].Surname,
+                PhoneNumber = users[0].PhoneNumber,
+                Mail = users[0].Mail,
+                HashPassword = users[0].HashPassword,
+                Salt = users[0].Salt,
+                Roles = users[0].Roles
+            };
         }
     }
 }
