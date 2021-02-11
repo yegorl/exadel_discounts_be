32523f28bf99546419c232a4fd8a3563c1124245
MACE <d.mace@live.com>
2021-02-11 16:04:15 +0300

    + added options for Protected CrazyPrice API

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.WebApi/Extentions/SwaggerExtention.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.WebApi/Extentions/SwaggerExtention.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using Exadel.CrazyPrice.WebApi.Filters;
 
 namespace Exadel.CrazyPrice.WebApi.Extentions
 {
@@ -19,12 +21,12 @@ namespace Exadel.CrazyPrice.WebApi.Extentions
         /// <returns></returns>
         public static IServiceCollection AddSwagger(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo
+                options.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "CrazyPrice API",
+                    Title = "Protected CrazyPrice API",
                     Description = "Description Web API for the site Crazy Price.",
                     License = new OpenApiLicense
                     {
@@ -33,10 +35,29 @@ namespace Exadel.CrazyPrice.WebApi.Extentions
                     }
                 });
 
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        AuthorizationCode = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri("https://localhost:8001/connect/authorize"),
+                            TokenUrl = new Uri("https://localhost:8001/connect/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                {"crazypriceapi", "CrazyPrice API - protected access"}
+                            }
+                        }
+                    }
+                });
+
+                options.OperationFilter<AuthorizeCheckOperationFilter>();
+
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+                options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
 
             return services;
@@ -50,7 +71,14 @@ namespace Exadel.CrazyPrice.WebApi.Extentions
         public static IApplicationBuilder UseSwaggerCrazyPrice(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CrazyPrice WebApi v1"));
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "CrazyPrice WebApi v1");
+
+                options.OAuthClientId("crazy_price_api_swagger");
+                options.OAuthAppName("Swagger UI for Crazy Price API");
+                options.OAuthUsePkce();
+            });
 
             return app;
         }
