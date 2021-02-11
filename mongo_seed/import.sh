dc90ab96ef284a0ac4d254498dc27a8db7187dcc
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-02-11 11:11:52 +0200

    *fixed import mock data to database

--- mongo_seed/import.sh
+++ mongo_seed/import.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
+#! /bin/sh
 
-mongoimport --host mongo --db CrazyPriceDb --collection Discounts --drop --jsonArray --batchSize 1 --file ./Discounts.json
-mongoimport --host mongo --db CrazyPriceDb --collection Users --drop --jsonArray --batchSize 1 --file ./Users.json
-mongoimport --host mongo --db CrazyPriceDb --collection Tags --drop --jsonArray --batchSize 1 --file ./Tags.json
+mongoimport --host mongo --db CrazyPriceDb --collection Discounts --drop --jsonArray --batchSize 1 --file /mongo_seed/Discounts.json
+mongoimport --host mongo --db CrazyPriceDb --collection Users --drop --jsonArray --batchSize 1 --file /mongo_seed/Users.json
+mongoimport --host mongo --db CrazyPriceDb --collection Tags --drop --jsonArray --batchSize 1 --file /mongo_seed/Tags.json
