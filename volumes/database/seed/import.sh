643ee1dac3dfe2afe2860e7f9c3d07dd18eb604b
MACE <d.mace@live.com>
2021-02-15 22:04:01 +0300

    * added Docker optimization

--- volumes/database/seed/import.sh
+++ volumes/database/seed/import.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-
-if [ $(mongo database:27017 --eval "db.getMongo().getDBNames().indexOf(\"CrazyPriceDb\")" --quiet) -lt 0 ]; then
+sleep 20
+INDEX=$(mongo database:27017 --eval "db.getMongo().getDBNames().indexOf('CrazyPriceDb')" --quiet)
+if [[ "$INDEX" -lt 0 ]]
+then
  mongoimport --host database --db CrazyPriceDb --collection Discounts --drop --jsonArray --batchSize 1 --file /seed/Discounts.json
  mongoimport --host database --db CrazyPriceDb --collection Users --drop --jsonArray --batchSize 1 --file /seed/Users.json
  mongoimport --host database --db CrazyPriceDb --collection Tags --drop --jsonArray --batchSize 1 --file /seed/Tags.json
