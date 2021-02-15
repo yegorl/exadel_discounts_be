2ad4e4cb2659bbd2d7cd949adaa5aa0d2baab014
MACE <d.mace@live.com>
2021-02-15 10:00:22 +0300

    * temporary changes

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/IdentityServer/Configuration/ConfigurationTests.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Exadel.CrazyPrice.Common.Models;
+using IdentityServer4;
+using IdentityServer4.Models;
+using Microsoft.OpenApi.Models;
+using Xunit;
+
+namespace Exadel.CrazyPrice.Tests.IdentityServer.Configuration
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void ClientJsonTest()
+        {
+            var value = new List<Client>
+            {
+                new Client
+                {
+                    AccessTokenType = AccessTokenType.Reference,
+                    // RequireConsent = false,
+                    AccessTokenLifetime = 330,// 330 seconds, default 60 minutes
+                    AllowAccessTokensViaBrowser = true,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "https://localhost:44357"
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "role",
+                        "crazy_price_api1"
+                    },
+
+                    ClientName = "vuejs_code_client",
+                    ClientId = "crazypriceclient",
+
+                    IdentityTokenLifetime = 300,
+
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:44357/",
+                        "https://localhost:44357"
+                    },
+
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44357",
+                        "https://localhost:44357/callback.html",
+                        "https://localhost:44357/silent-renew.html"
+                    }
+                },
+                new Client
+                {
+                    AccessTokenType = AccessTokenType.Reference,
+                    AccessTokenLifetime = 120,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "role",
+                        "crazy_price_api1"
+                    },
+                    AllowOfflineAccess = true,
+
+                    ClientId = "crazypricetestclient",
+                    ClientName = "Crazy Price Test",
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:44325/signout-callback-oidc",
+                    },
+
+                    RequirePkce = true,
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44325",
+                        "https://localhost:44325/signin-oidc",
+                    },
+
+                    UpdateAccessTokenClaimsOnRefresh = true
+                },
+                new Client
+                {
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "crazy_price_api1" },
+
+                    // no interactive user, use the clientid/secret for authentication
+                    ClientId = "crazy_price_api",
+                    ClientName = "Crazy Price API",
+                    ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        }
+                },
+                new Client
+                {
+                    AccessTokenType = AccessTokenType.Reference,
+                    AllowedCorsOrigins = { "https://localhost:44389" },
+                    AllowedGrantTypes = GrantTypes.ImplicitAndClientCredentials,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "role",
+                        "crazy_price_api1"
+                    },
+
+                    ClientId = "crazy_price_api_swagger",
+                    ClientName = "Swagger UI for Crazy Price API",
+                    //ClientSecrets =
+                    //{
+                    //    new Secret("secret".Sha256())
+                    //},
+
+                    RedirectUris = { "https://localhost:44389/swagger/oauth2-redirect.html" },
+                    //RedirectUris = { "https://localhost:9001/swagger/oauth2-redirect.html" },
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+
+                    PostLogoutRedirectUris = { "https://localhost:44389/signout-callback-oidc" },
+                    //PostLogoutRedirectUris = { "https://localhost:9001/signout-callback-oidc" },
+
+                    UpdateAccessTokenClaimsOnRefresh = true
+                }
+                };
+
+            //var value = new Secret("secret".Sha256());
+
+            //var value =
+            //    new[]
+            //    {
+            //        new IdentityResources.OpenId(),
+            //        new IdentityResources.Profile(),
+            //        new IdentityResource("role", "Your role(s)", new List<string> { "role" })
+            //    };
+
+            //var value = new OpenApiOAuthFlows
+            //{
+            //    AuthorizationCode = new OpenApiOAuthFlow
+            //    {
+            //        AuthorizationUrl = new Uri("https://identityserver:443/connect/authorize"),
+            //        TokenUrl = new Uri("https://identityserver:443/connect/token"),
+
+            //        Scopes = new Dictionary<string, string>
+            //    {
+            //        {"crazy_price_api1", "CrazyPrice API - protected access"}
+            //    }
+            //    }
+            //};
+
+            var jsonString = JsonSerializer.Serialize(value);
+
+            //Assert
+            Assert.IsType<List<Client>>(jsonString);
+        }
+    }
+}
--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/WebApi/Controllers/DiscountsControllerTests.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/WebApi/Controllers/DiscountsControllerTests.cs
@@ -1,161 +1,161 @@
-﻿using Exadel.CrazyPrice.Common.Interfaces;
-using Exadel.CrazyPrice.Common.Models.Request;
-using Exadel.CrazyPrice.Common.Models.Response;
-using Exadel.CrazyPrice.Common.Models.SearchCriteria;
-using Exadel.CrazyPrice.WebApi.Controllers;
-using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Exadel.CrazyPrice.Common.Models;
-using Exadel.CrazyPrice.Common.Models.Option;
-using Xunit;
+﻿//using Exadel.CrazyPrice.Common.Interfaces;
+//using Exadel.CrazyPrice.Common.Models.Request;
+//using Exadel.CrazyPrice.Common.Models.Response;
+//using Exadel.CrazyPrice.Common.Models.SearchCriteria;
+//using Exadel.CrazyPrice.WebApi.Controllers;
+//using FluentAssertions;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using Moq;
+//using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Exadel.CrazyPrice.Common.Models;
+//using Exadel.CrazyPrice.Common.Models.Option;
+//using Xunit;
 
-namespace Exadel.CrazyPrice.Tests.WebApi.Controllers
-{
-    public class DiscountsControllerTests
-    {
-        private readonly Mock<ILogger<DiscountsController>> _mockLogger;
-        private readonly Mock<IDiscountRepository> _mockRepository;
+//namespace Exadel.CrazyPrice.Tests.WebApi.Controllers
+//{
+//    public class DiscountsControllerTests
+//    {
+//        private readonly Mock<ILogger<DiscountsController>> _mockLogger;
+//        private readonly Mock<IDiscountRepository> _mockRepository;
 
-        public DiscountsControllerTests()
-        {
-            _mockLogger = new Mock<ILogger<DiscountsController>>();
-            _mockRepository = new Mock<IDiscountRepository>();
-        }
+//        public DiscountsControllerTests()
+//        {
+//            _mockLogger = new Mock<ILogger<DiscountsController>>();
+//            _mockRepository = new Mock<IDiscountRepository>();
+//        }
 
-        [Fact]
-        public async Task GetDiscountOkTest()
-        {
-            var searchValue = Guid.NewGuid();
-            var resultValues = new Discount()
-            {
-                Id = searchValue
-            };
+//        [Fact]
+//        public async Task GetDiscountOkTest()
+//        {
+//            var searchValue = Guid.NewGuid();
+//            var resultValues = new Discount()
+//            {
+//                Id = searchValue
+//            };
 
-            _mockRepository.Setup(r => r.GetDiscountByUidAsync(searchValue))
-                .ReturnsAsync(resultValues);
+//            _mockRepository.Setup(r => r.GetDiscountByUidAsync(searchValue))
+//                .ReturnsAsync(resultValues);
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.GetDiscount(searchValue, LanguageOption.Ru);
+//            var actionResult = await controller.GetDiscount(searchValue, LanguageOption.Ru);
 
-            var result = Assert.IsType<OkObjectResult>(actionResult);
-            var returnValue = Assert.IsType<DiscountResponse>(result.Value);
+//            var result = Assert.IsType<OkObjectResult>(actionResult);
+//            var returnValue = Assert.IsType<DiscountResponse>(result.Value);
 
-            returnValue.Should().NotBeNull();
-        }
+//            returnValue.Should().NotBeNull();
+//        }
 
-        [Fact]
-        public async Task GetDiscountNotFoundTest()
-        {
-            var searchValue = Guid.NewGuid();
-            var resultValues = (Discount)null;
+//        [Fact]
+//        public async Task GetDiscountNotFoundTest()
+//        {
+//            var searchValue = Guid.NewGuid();
+//            var resultValues = (Discount)null;
 
-            _mockRepository.Setup(r => r.GetDiscountByUidAsync(searchValue))
-                .ReturnsAsync(resultValues);
+//            _mockRepository.Setup(r => r.GetDiscountByUidAsync(searchValue))
+//                .ReturnsAsync(resultValues);
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.GetDiscount(searchValue, LanguageOption.Ru);
+//            var actionResult = await controller.GetDiscount(searchValue, LanguageOption.Ru);
 
-            var result = Assert.IsType<NotFoundObjectResult>(actionResult);
-            var returnValue = Assert.IsType<string>(result.Value);
+//            var result = Assert.IsType<NotFoundObjectResult>(actionResult);
+//            var returnValue = Assert.IsType<string>(result.Value);
 
-            returnValue.Should().BeEquivalentTo("No discount found.");
-        }
+//            returnValue.Should().BeEquivalentTo("No discount found.");
+//        }
 
-        [Fact]
-        public async Task GetDiscountsOkTest()
-        {
-            var searchValue = new SearchCriteria();
-            var resultValues = new List<Discount>
-            {
-                new Discount()
-            };
+//        [Fact]
+//        public async Task GetDiscountsOkTest()
+//        {
+//            var searchValue = new SearchCriteria();
+//            var resultValues = new List<Discount>
+//            {
+//                new Discount()
+//            };
 
-            _mockRepository.Setup(r => r.GetDiscountsAsync(searchValue))
-                .ReturnsAsync(resultValues);
+//            _mockRepository.Setup(r => r.GetDiscountsAsync(searchValue))
+//                .ReturnsAsync(resultValues);
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.GetDiscounts(searchValue);
+//            var actionResult = await controller.GetDiscounts(searchValue);
 
-            var result = Assert.IsType<OkObjectResult>(actionResult);
-            var returnValue = Assert.IsType<List<DiscountResponse>>(result.Value);
+//            var result = Assert.IsType<OkObjectResult>(actionResult);
+//            var returnValue = Assert.IsType<List<DiscountResponse>>(result.Value);
 
-            returnValue.Should().NotBeNull();
-        }
+//            returnValue.Should().NotBeNull();
+//        }
 
-        [Fact]
-        public async Task GetDiscountsNotFoundTest()
-        {
-            var searchValue = new SearchCriteria();
-            var resultValues = (List<Discount>)null;
+//        [Fact]
+//        public async Task GetDiscountsNotFoundTest()
+//        {
+//            var searchValue = new SearchCriteria();
+//            var resultValues = (List<Discount>)null;
 
-            _mockRepository.Setup(r => r.GetDiscountsAsync(searchValue))
-                .ReturnsAsync(resultValues);
+//            _mockRepository.Setup(r => r.GetDiscountsAsync(searchValue))
+//                .ReturnsAsync(resultValues);
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.GetDiscounts(searchValue);
+//            var actionResult = await controller.GetDiscounts(searchValue);
 
-            var result = Assert.IsType<NotFoundObjectResult>(actionResult);
-            var returnValue = Assert.IsType<string>(result.Value);
+//            var result = Assert.IsType<NotFoundObjectResult>(actionResult);
+//            var returnValue = Assert.IsType<string>(result.Value);
 
-            returnValue.Should().BeEquivalentTo("No discounts found.");
-        }
+//            returnValue.Should().BeEquivalentTo("No discounts found.");
+//        }
         
-        [Fact]
-        public async Task UpsertDiscountBadRequestTest()
-        {
-            var searchValue = new Discount();
-            var resultValues = (Discount)null;
+//        [Fact]
+//        public async Task UpsertDiscountBadRequestTest()
+//        {
+//            var searchValue = new Discount();
+//            var resultValues = (Discount)null;
 
-            _mockRepository.Setup(r => r.UpsertDiscountAsync(searchValue))
-                .ReturnsAsync(resultValues);
+//            _mockRepository.Setup(r => r.UpsertDiscountAsync(searchValue))
+//                .ReturnsAsync(resultValues);
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.UpsertDiscount(searchValue);
+//            var actionResult = await controller.UpsertDiscount(searchValue);
 
-            var result = Assert.IsType<BadRequestObjectResult>(actionResult);
-            var returnValue = Assert.IsType<string>(result.Value);
+//            var result = Assert.IsType<BadRequestObjectResult>(actionResult);
+//            var returnValue = Assert.IsType<string>(result.Value);
 
-            returnValue.Should().BeEquivalentTo("No discounts were create or update.");
-        }
+//            returnValue.Should().BeEquivalentTo("No discounts were create or update.");
+//        }
 
-        [Fact]
-        public async Task DeleteDiscountOkTest()
-        {
-            var searchValue = Guid.NewGuid();
-            var userId = Guid.Parse("0aed5f3e-3c8b-434d-adb0-e75bbca32b38");
+//        [Fact]
+//        public async Task DeleteDiscountOkTest()
+//        {
+//            var searchValue = Guid.NewGuid();
+//            var userId = Guid.Parse("0aed5f3e-3c8b-434d-adb0-e75bbca32b38");
 
-            _mockRepository.Setup(r => r.RemoveDiscountByUidAsync(searchValue, userId));
+//            _mockRepository.Setup(r => r.RemoveDiscountByUidAsync(searchValue, userId));
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.DeleteDiscount(searchValue);
+//            var actionResult = await controller.DeleteDiscount(searchValue);
 
-            Assert.IsType<OkResult>(actionResult);
-        }
+//            Assert.IsType<OkResult>(actionResult);
+//        }
 
-        [Fact]
-        public async Task DeleteDiscountsOkTest()
-        {
-            var searchValue = new List<Guid>();
-            var userId = Guid.Parse("0aed5f3e-3c8b-434d-adb0-e75bbca32b38");
+//        [Fact]
+//        public async Task DeleteDiscountsOkTest()
+//        {
+//            var searchValue = new List<Guid>();
+//            var userId = Guid.Parse("0aed5f3e-3c8b-434d-adb0-e75bbca32b38");
 
-            _mockRepository.Setup(r => r.RemoveDiscountAsync(searchValue, userId));
+//            _mockRepository.Setup(r => r.RemoveDiscountAsync(searchValue, userId));
 
-            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
+//            var controller = new DiscountsController(_mockLogger.Object, _mockRepository.Object);
 
-            var actionResult = await controller.DeleteDiscounts(searchValue);
+//            var actionResult = await controller.DeleteDiscounts(searchValue);
 
-            Assert.IsType<OkResult>(actionResult);
-        }
-    }
-}
+//            Assert.IsType<OkResult>(actionResult);
+//        }
+//    }
+//}
