1c619c6786aa1ed90490e9c2ca59775a11f121a3
MACE <d.mace@live.com>
2021-02-17 19:21:56 +0300

    + added MongoDbConfigurationTests

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Data/Configuration/MongoDbConventionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exadel.CrazyPrice.Data.Configuration;
+using FluentAssertions;
+using Xunit;
+
+namespace Exadel.CrazyPrice.Tests.Data.Configuration
+{
+    public class MongoDbConventionTests
+    {
+        [Fact]
+        public void MongoDbConventionTest()
+        {
+            Action action = () => MongoDbConvention.SetCamelCaseElementNameConvention();
+            action.Should().NotThrow();
+        }
+    }
+}
