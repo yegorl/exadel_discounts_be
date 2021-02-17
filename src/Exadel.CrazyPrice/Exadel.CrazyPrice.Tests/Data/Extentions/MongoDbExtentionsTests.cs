89e484c4cb36373bdbae6dce4d67b33c5a8490c7
MACE <d.mace@live.com>
2021-02-17 20:05:11 +0300

    + added MongoDbExtentionsTests

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Data/Extentions/MongoDbExtentionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exadel.CrazyPrice.Data.Configuration;
+using Exadel.CrazyPrice.Data.Extentions;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Exadel.CrazyPrice.Tests.Data.Extentions
+{
+    public class MongoDbExtentionsTests
+    {
+        [Fact]
+        public void MongoDbExtentionsTest()
+        {
+            IServiceCollection _service = new ServiceCollection();
+            _service.AddMongoDb();
+            var mainService = _service.FirstOrDefault(d => d.ImplementationType == typeof(MongoDbConfiguration));
+            mainService.Should().NotBeNull();
+        }
+    }
+}
