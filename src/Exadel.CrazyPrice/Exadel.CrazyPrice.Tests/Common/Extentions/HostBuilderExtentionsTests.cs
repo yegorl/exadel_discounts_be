b0bc882351bf3c14a4500998798f021a20e04973
MACE <d.mace@live.com>
2021-02-18 12:50:14 +0300

    * improved test HostBuilderExtentionsTests

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Extentions/HostBuilderExtentionsTests.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Extentions/HostBuilderExtentionsTests.cs
@@ -1,7 +1,9 @@
 ﻿using Exadel.CrazyPrice.Common.Extentions;
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Exadel.CrazyPrice.Tests.Common.Extentions
@@ -9,11 +11,22 @@ namespace Exadel.CrazyPrice.Tests.Common.Extentions
     public class HostBuilderExtentionsTests
     {
         [Fact]
-        public void GetArrayStringTest()
+        public void SetupLoggerTest()
         {
-            IHostBuilder builder = new HostBuilder();
+            var builder = new HostBuilder().ConfigureAppConfiguration(configBuilder =>
+            {
+                configBuilder
+                    .AddInMemoryCollection(new KeyValuePair<string, string>[]
+                    {
+                        new("Logging", "enabled"),
+                        new("LogToSerilog", "true"),
+                        new("LogToNLog","true")
+                    })
+                    .Build();
+            });
 
-            Action action = () => builder.SetupLogger();
+            Action action = () => builder.SetupLogger().Build();
+
             action.Should().NotThrow();
         }
     }
