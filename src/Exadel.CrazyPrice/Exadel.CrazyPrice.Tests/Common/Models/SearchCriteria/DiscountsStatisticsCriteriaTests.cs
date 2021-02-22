d654bd4e60cfa8859c2d30371ce27414f4d7cf84
MACE <d.mace@live.com>
2021-02-22 17:06:31 +0300

    + added DiscountsStatisticsCriteriaTest

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.Tests/Common/Models/SearchCriteria/DiscountsStatisticsCriteriaTests.cs
@@ -0,0 +1,27 @@
+﻿using Exadel.CrazyPrice.Common.Extentions;
+using Exadel.CrazyPrice.Common.Models.SearchCriteria;
+using FluentAssertions;
+using Xunit;
+
+namespace Exadel.CrazyPrice.Tests.Common.Models.SearchCriteria
+{
+    public class DiscountsStatisticsCriteriaTests
+    {
+        [Fact]
+        public void DiscountsStatisticsCriteriaTest()
+        {
+            var discountsStatisticsCriteria = new DiscountsStatisticsCriteria()
+            {
+                CreateStartDate = "01.01.2020 10:10:10".GetUtcDateTime(),
+                CreateEndDate = "01.01.2021 10:10:10".GetUtcDateTime(),
+                SearchAddressCity = "SearchAddressCity",
+                SearchAddressCountry = "SearchAddressCountry"
+            };
+
+            discountsStatisticsCriteria.SearchAddressCountry.Should().NotBeNull();
+            discountsStatisticsCriteria.SearchAddressCity.Should().NotBeNull();
+            discountsStatisticsCriteria.CreateEndDate.Should().NotBeNull();
+            discountsStatisticsCriteria.CreateStartDate.Should().NotBeNull();
+        }
+    }
+}
