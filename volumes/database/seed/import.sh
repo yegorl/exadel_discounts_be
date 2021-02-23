31cc46dd183bc257e27bd113a97114fe00eaf766
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-02-23 18:29:58 +0200

    + added new configurations and updated data for seedding into containers

--- volumes/database/seed/import.sh
+++ volumes/database/seed/import.sh
@@ -3,6 +3,7 @@ sleep 20
 INDEX=$(mongo database:27017 --eval "db.getMongo().getDBNames().indexOf('CrazyPriceDb')" --quiet)
 if [[ "$INDEX" -lt 0 ]]
 then
+ mongoimport --host database --db CrazyPriceDb --collection ExternalUsers --drop --jsonArray --batchSize 1 --file /seed/ExternalUsers.json
  mongoimport --host database --db CrazyPriceDb --collection Discounts --drop --jsonArray --batchSize 1 --file /seed/Discounts.json
  mongoimport --host database --db CrazyPriceDb --collection Users --drop --jsonArray --batchSize 1 --file /seed/Users.json
  mongoimport --host database --db CrazyPriceDb --collection Tags --drop --jsonArray --batchSize 1 --file /seed/Tags.json
