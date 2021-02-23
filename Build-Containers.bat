69cddb793ff4905b35f418e826852bc07780cb60
MACE <d.mace@live.com>
2021-02-23 20:27:18 +0300

    * added archive and automation of actions

--- Build-Containers.bat
+++ Build-Containers.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if not exist "volumes/database/seed/Discounts.json" (call Extract-DB-FakeData.bat)
+
 echo Build Docker containers
 docker-compose up --build
 pause
---
+++ Extract-DB-FakeData.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start volumes/utils/7za.exe x volumes/database/seed/fakedata.zip -ovolumes/database/seed/
