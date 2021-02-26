f617f27324b73e9d429630c057b55297cfee5973
victor.shevchenko.net@gmail.com <victor.shevchenko.net@gmail.com>
2021-02-26 18:39:39 +0200

    + fixed error when extenal user tried authorize via login form

--- src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/Services/CryptographicService.cs
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.IdentityServer/Services/CryptographicService.cs
@@ -6,12 +6,12 @@ namespace Exadel.CrazyPrice.IdentityServer.Services
 {
     public class CryptographicService : ICryptographicService
     {
-        private const int SaltSize = 30;
         private const int HashSize = 128;
         private const int IterationCount = 20;
         public bool ComparePasswordHash(string password, string hashedPassword, string salt)
         {
             var saltByte = Convert.FromBase64String(salt);
+            var result = false;
             byte[] testHash;
 
             //generate hash from test password and original salt and iterations
@@ -20,14 +20,20 @@ namespace Exadel.CrazyPrice.IdentityServer.Services
                 testHash = rfc2898DeriveBytes.GetBytes(HashSize);
             }
 
-            //if hash values match then return success
-            if (Convert.ToBase64String(testHash) == hashedPassword)
+            try
             {
-                return true;
+                //if hash values match then return success
+                if (Convert.ToBase64String(testHash) == hashedPassword)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
             }
 
-            //no match return false
-            return false;
+            return result;
         }
     }
 }
