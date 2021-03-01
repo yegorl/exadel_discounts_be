892cd46905d58e829c7dc9cd25da1f09d92d42ad
MACE <d.mace@live.com>
2021-03-02 00:32:05 +0300

    + added ApplicationInfo

---
+++ src/Exadel.CrazyPrice/Exadel.CrazyPrice.WebApi/Helpers/ApplicationInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Exadel.CrazyPrice.WebApi.Helpers
+{
+    public static class ApplicationInfo
+    {
+        public static string ApplicationName
+        {
+            get
+            {
+                var startupNamespace = typeof(Startup).Namespace;
+                return startupNamespace!.Substring(startupNamespace.LastIndexOf('.', startupNamespace.LastIndexOf('.') - 1) + 1);
+            }
+        }
+    }
+}
